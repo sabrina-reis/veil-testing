20f8f1d865486ee6ae870ad11bc9fd4f3ba6d721
Sabrina Reis <sreis@uoregon.edu>
2026-06-05 11:17:34 -0700

    finished actions

--- veil-lean-files/examples/cykas.lean
+++ veil-lean-files/examples/cykas.lean
@@ -35,7 +35,6 @@ to `msg_receiver`?
 relation eager_msg (sender : process) (receiver : process) (m : message)
 relation normal_msg (sender : process) (receiver : process) (m : message)
 relation ack_msg (sender : process) (receiver : process) (m : message)
-relation youcantell_msg (sender : process) (secret_receiver : process) (msg_receiver : process) (m : message)
 
 /-
 State of the processes
@@ -70,7 +69,6 @@ after_init {
   eager_msg S R M := False;
   normal_msg S R M := False;
   ack_msg S R M := False;
-  youcantell_msg S R M V := False;
 }
 
 /-
@@ -85,7 +83,22 @@ process state to indicate that the sender is currently sending a message.
 action normal_send (sender : process) (receiver : process) (m : message) = {
   require ¬ secret receiver ∧ ∀ R M, ¬ sending sender R M;
   normal_msg sender receiver m := True;
+  delivered sender receiver m := False;
   sending sender receiver m := True;
+}
+
+/-
+Conditions:
+To deliver a normal message, a corresponding normal message must already exist.
+
+If met:
+  Mark the message as delivered.
+  Mark the sender as no longer sending this message.
+-/
+action normal_delivery (sender : process) (receiver : process) (m : message) = {
+  require normal_msg sender receiver m;
+  sending sender receiver m := False;
+  delivered sender receiver m := True;
 }
 
 /-
@@ -98,6 +111,7 @@ Initialize an eager message and put the receiver into secret mode.
 action eager_send (sender : process) (receiver : process) (m : message) = {
   require ∃ R M, sending sender R M;
   eager_msg sender receiver m := True;
+  delivered sender receiver m := False;
   secret receiver := True;
   -- receiver could already be in secret mode; is that a problem?
 
@@ -105,67 +119,68 @@ action eager_send (sender : process) (receiver : process) (m : message) = {
 
 /-
 Conditions:
-To deliver a normal message, a corresponding normal message must already exist.
+To deliver an eager message, a corresponding eager message must already exist.
 
 If met:
-  Mark the message as delivered e as having echoed a message in this round
-  Mark the echoed message as true.
+  Mark the message as delivered.
+  Mark the sender as no longer sending this message.
 -/
-action normal_delivery (sender : process) (receiver : process) (m : message) = {
-  sorry
-}
-
-/-
-SabNote:
-From reliable_broadcast
-
-Conditions:
-To deliver a message...
-  A supermajority of nodes must have voted for the the message, AND
-  if the source node of the message is a member of the nodeset, then the
-  message from the source node to n must have already been voted on.
-
-If met:
-  Mark the node as having delivered a message.
--/
-action deliver (n : address) (originator : address) (r : round) (v : message) = {
-  -- received 2f + 1 votes
-  require (∃ (q : nodeset), nset.supermajority q ∧
-              ∀ (src : address), nset.member src q → vote_msg src n originator r v);
-  delivered n originator r v := True
-}
-
 action eager_delivery (sender : process) (receiver : process) (m : message) = {
-  sorry
-}
-
-action ack (sender : process) (receiver : process) (m : message) = {
-  sorry
-}
-
-action you_can_tell (sender : process) (receiver : process) (m : message) = {
-  sorry
+  require eager_msg sender receiver m;
+  sending sender receiver m := False;
+  delivered sender receiver m := True;
 }
 
 /-
-From reliable_broadcast
-SabNote:
-The safety conditions here are checking for consistency of message contents.
+Conditions:
+To ACK an eager message,
+  A corresponding eager message must already exist.
+  The sender must no longer be sending the eager message.
+  This eager message must already have been delivered.
+
+If met:
+  Mark the message as delivered.
+  Mark the sender as no longer sending this message.
 -/
-/- If a message is voted for, it is the message that was initially proposed by the originator. -/
-safety [vote_integrity]
-  ∀ (src dst : address) (r : round) (v : message),
-     ¬ is_byz src ∧ ¬ is_byz dst ∧ voted dst src r v → (sent src r ∧ initial_message src r v)
+action ack (sender : process) (receiver : process) (m : message) = {
+  require eager_msg sender receiver m;
+  require ¬ sending sender receiver m;
+  require delivered sender receiver m;
+
+  ack_msg sender receiver m := True;
+}
+
+-- ack_msg: has the message `m` from `sender` to `receiver` been acknowledged?
+
+/-
+Conditions:
+To tell a process in secret mode that they can tell,
+  The eager message that put the process in secret mode must have been ACKed.
+
+If met:
+  Take the secret receiver out of secret mode.
+  (TODO: I think this is enough to allow them to perform their send transition?)
+-/
+action you_can_tell (sender : process) (secret_receiver : process) (msg_receiver : process) (m : message) = {
+  require ack_msg sender msg_receiver m;
+  secret secret_receiver := False;
+}
+
+/-
+From Cykas paper:
+The safety property we wish to ensure is causal delivery, i.e., messages are
+never delivered in an order that violates the causal order.
+
+The liveness property we wish to ensure is that, assuming a reliable network,
+all messages will eventually be delivered.
+-/
+safety [causal_delivery]
+  sorry
 
 /- If a message is delivered, it is the message that was initially proposed by the originator. -/
-safety [deliver_integrity]
-  ∀ (src dst : address) (r : round) (v : message),
-     ¬ is_byz src ∧ ¬ is_byz dst ∧ delivered dst src r v → (sent src r ∧ initial_message src r v)
 
-/- Also known as "delivered uniqueness". -/
-safety [agreement]
-  ∀ (src dst₁ dst₂ : address) (r : round) (v₁ v₂ : message),
-    ¬ is_byz dst₁ ∧ ¬ is_byz dst₂ ∧ delivered dst₁ src r v₁ ∧ delivered dst₂ src r v₂ → v₁ = v₂
+invariant[reliable_delivery]
+  sorry
 
 #gen_spec
 
